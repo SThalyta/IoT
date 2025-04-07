c8bc1f33232b55a49db95cb75975dd0516c3329a
Thalyta <ssousathalyta@gmail.com>
2025-04-07 16:16:20 -0300

    Adicionando a funcionalidade de alteração do estado da lâmpada

--- CoAP/c_put_lamp.py
+++ CoAP/c_put_lamp.py
@@ -1,15 +1,20 @@
 import asyncio
 from aiocoap import Context, Message, PUT
+import argparse
 
-async def main():
+async def main(payload):
     context = await Context.create_client_context()
-
-    payload = b"off" 
-    request = Message(code=PUT, payload=payload, uri="coap://127.0.0.1/lampada")
+    
+    request = Message(code=PUT, payload=payload.encode('utf-8'), uri="coap://127.0.0.1/lampada")
 
     response = await context.request(request).response
 
     print(f"Agora o estado da lampada é: {response.payload.decode('utf-8')}")
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--payload", required= True, help='Estado da lâmpada')
+
+    args = parser.parse_args()
+
+    asyncio.run(main(args.payload))
